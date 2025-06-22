398f3663bc9de7a33e0392067a184aee35fe6c46
Ezequiel Mareco <ezequielmareco33@gmail.com>
2025-06-21 22:08:51 -0300

    dataBound inicial en AsignarTurnos y comienzo de desarrollo de clases Turno, gestion y negocio Turno

---
+++ Datos/GestionTurnos.cs
@@ -0,0 +1,37 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Data;
+
+
+namespace Datos
+{
+    public class GestionTurnos
+    {
+        //CONEXION A LA BASE DE DATOS
+        private string Conexion = @"Data Source=localhost\SQLEXPRESS; Initial Catalog=ClinicaGrupo15;Integrated Security = True";
+
+        //OBJETO DE LA CLASE TURNOS
+        Turnos _Turno = new Turnos();
+
+        //OBTENER CONEXION
+        public SqlConnection ObtenerConexion()
+        {
+            try
+            {
+                SqlConnection conexion = new SqlConnection(Conexion);
+                return conexion;
+            }
+            catch (Exception)
+            {
+                return null; // Retorna null si hay un error al crear la conexión
+            }
+
+        }
+    }
+}
